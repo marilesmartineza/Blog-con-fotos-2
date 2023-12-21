731bb3a1ac391b90bb9d3a8da06e824a80715f42
Ale Mariles <97320635+marilesmartineza@users.noreply.github.com>
2023-12-20 18:17:29 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+function setDate() {
+    document.getElementById("date").innerHTML = new Date().toDateString();
+}
+
+function alerta() {
+    window.alert("Cámara mi gente, yo soy la desarrolladora :v");
+  }
